b376dc5d051280b92e3bccde3689f13ddf8e6fe3
片山　朋和 <poko.peko.tomo@icloud.com>
2021-11-16 23:17:06 +0900

    11/16

--- sketch_nov02a/sketch_nov02a.ino
+++ sketch_nov02a/sketch_nov02a.ino
@@ -29,6 +29,8 @@ const int S3_Th = 512; // センサー閾値
 String RESET = String("reset");
 Sleep sleep;
 unsigned long sleepTime = 8.64e+7;
+int search_line_count;
+
 
 struct sensors {
   bool A;
@@ -79,9 +81,57 @@ void setup() {
 
 void loop() {
   sensor_state = Sensor();
-  if (sensor_state.A + sensor_state.B + sensor_state.C + sensor_state.D) {
-    Back(500);
+  struct sensors before_state;
+  if (sensors_status_equal(sensor_state, before_state)) Serial.println("Status was changed.");
+
+  if (sensors_status_equal(sensor_state, {0, 0, 0, 0})) {
+    Serial.println("Line No Found");
+    User_reset();
+    if (search_line_count < 4) {
+      SuperLeft(100);
+      Stop();
+      Pause(40);
+      search_line_count++;
+    } else if (search_line_count < 12) {
+      SuperRight(100);
+      Stop();
+      Pause(40);
+      search_line_count++;
+    } else if (search_line_count < 16) {
+      SuperLeft(100);
+      Stop();
+      Pause(40);
+      search_line_count++;
+    } else {
+      Stop();
+      Pause(2000);
+    }
+  } else {
+    search_line_count = 0;
   }
+
+
+  if (sensors_status_equal(sensor_state, {1, 0, 0, 0}) || sensors_status_equal(sensor_state, {1, 1, 0, 0})) {
+    Left(50);
+    Serial.print("A: ");
+    Serial.println("1\n\n\n");
+    Sensor();
+
+    //Left(100);
+  } else if (sensors_status_equal(sensor_state, {0, 0, 0, 1}) || sensors_status_equal(sensor_state, {0, 0, 0, 0})) {
+    Right(50);
+    Serial.print("D: ");
+    Serial.println("1\n\n\n");
+    Sensor();
+
+    //Right(100);
+  }
+  //Pause(200);
+
+
+
+
+  before_state = sensor_state;
   User_reset();
 
 
@@ -107,7 +157,7 @@ void loop() {
 
 struct sensors Sensor() {
   int S0, S1, S2, S3;
-  struct sensors sensor_status = {0, 0, 0, 0};
+  struct sensors sensor_status = {1, 1, 1, 1};
   S0 = analogRead(pinS0);
   Serial.print("Sensor S0: ");
   Serial.println(S0);
@@ -120,12 +170,23 @@ struct sensors Sensor() {
   S3 = analogRead(pinS3);
   Serial.print("Sensor S3: ");
   Serial.println(S3);
-  if (Sensor_Threshold < S0) sensor_status.A = 1;
-  if (Sensor_Threshold < S1) sensor_status.B = 1;
-  if (Sensor_Threshold < S2) sensor_status.C = 1;
-  if (Sensor_Threshold < S3) sensor_status.D = 1;
-  Serial.print(sensor_status.A);
+  if (Sensor_Threshold < S0) sensor_status.A = 0;
+  if (Sensor_Threshold < S1) sensor_status.B = 0;
+  if (Sensor_Threshold < S2) sensor_status.C = 0;
+  if (Sensor_Threshold < S3) sensor_status.D = 0;
+  Serial.print("sensor_status.A: ");
+  Serial.println(sensor_status.A);
+  Serial.print("sensor_status.B: ");
+  Serial.println(sensor_status.B);
+  Serial.print("sensor_status.C: ");
+  Serial.println(sensor_status.C);
+  Serial.print("sensor_status.D: ");
+  Serial.println(sensor_status.D);
   return sensor_status;
+}
+
+bool sensors_status_equal(struct sensors a, struct sensors b) {
+  return a.A == b.A && a.B == b.B && a.C == b.C && a.D == b.D;
 }
 void Motor(bool M0_IN1, bool M0_IN2, bool M1_IN1, bool M1_IN2) {
   analogWrite(pinM0_IN1, M0_IN1 * SPEED);
@@ -148,105 +209,119 @@ void SuperStop() {
 void Left(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\n Left in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(false, true, false, false);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void SuperLeft(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\n SuperLeft in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
-    Motor(true, false, false, true);
+    Motor(false, true, false, true);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void BackLeft(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\nBackLeft in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(false, false, false, true);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void Right(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\n Right in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(false, false, true, false);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void SuperRight(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\nSuperRight in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
-    Motor(false, true, true, false);
+    Motor(true, false, true, false);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void BackRight(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\nBackRight in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(false, true, false, false);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void Back(long  ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\nBack in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(true, false, false, true);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
 }
 
 void Straight(long ms) {
   long  time = millis() + ms;
   Serial.print("\n\n\n\nStraight in ");
-  Serial.println("time:");
-  Serial.print(time);
+  Serial.print("time:");
+  Serial.println(time);
   while (millis() < time + ms) {
     Motor(false, true, true, false);
     User_reset();
   }
-  Stop();
-  delay(70);
+  //Stop();
+  //delay(70);
+}
+
+
+
+void Pause(long ms) {
+  long  time = millis() + ms;
+  Serial.print("\n\n\n\nPause in ");
+  Serial.print("time:");
+  Serial.println(time);
+  while (millis() < time + ms) {
+    User_reset();
+  }
+  //Stop();
+  //delay(70);
 }
 
 
